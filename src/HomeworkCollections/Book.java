98e8557a55385e8021198a0a318ad044bc3f4bc3
romanovskaya_a <medova9an9@gmail.com>
2019-10-21 17:28:45 +0300

    Дз коллекции

---
+++ src/HomeworkCollections/Book.java
@@ -0,0 +1,106 @@
+package HomeworkCollections;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String authorName;
+    private String authorSurname;
+    private String authorSecondName;
+    private int theDateOfPublishing;
+    private int pages;
+
+    public Book(String title, String authorName, int theDateOfPublishing, int pages) {
+        this.title = title;
+        this.authorName = authorName;
+        this.theDateOfPublishing = theDateOfPublishing;
+        this.pages = pages;
+    }
+
+    public Book(String title, String authorName, String authorSurname, String authorSecondName, int theDateOfPublishing, int pages) {
+        this.title = title;
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
+        this.authorSecondName = authorSecondName;
+        this.theDateOfPublishing = theDateOfPublishing;
+        this.pages = pages;
+    }
+
+    public String getAuthorSurname() {
+        return authorSurname;
+    }
+
+    public void setAuthorSurname(String authorSurname) {
+        this.authorSurname = authorSurname;
+    }
+
+    public String getAuthorSecondName() {
+        return authorSecondName;
+    }
+
+    public void setAuthorSecondName(String authorSecondName) {
+        this.authorSecondName = authorSecondName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String author) {
+        this.authorName = author;
+    }
+
+    public int getTheDateOfPublishing() {
+        return theDateOfPublishing;
+    }
+
+    public void setTheDateOfPublishing(int theDateOfPublishing) {
+        this.theDateOfPublishing = theDateOfPublishing;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return getTheDateOfPublishing() == book.getTheDateOfPublishing() &&
+                getPages() == book.getPages() &&
+                Objects.equals(getTitle(), book.getTitle()) &&
+                Objects.equals(getAuthorName(), book.getAuthorName()) &&
+                Objects.equals(getAuthorSurname(), book.getAuthorSurname()) &&
+                Objects.equals(getAuthorSecondName(), book.getAuthorSecondName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getAuthorName(), getAuthorSurname(), getAuthorSecondName(), getTheDateOfPublishing(), getPages());
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", authorName='" + authorName + '\'' +
+                ", authorSurname='" + authorSurname + '\'' +
+                ", authorSecondName='" + authorSecondName + '\'' +
+                ", theDateOfPublishing=" + theDateOfPublishing +
+                ", pages=" + pages +
+                '}';
+    }
+}
---
+++ src/HomeworkCollections/mainClass.java
@@ -0,0 +1,63 @@
+package HomeworkCollections;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class mainClass {
+    public static void main(String[] args) {
+        System.out.println("1. Создать экземпляр класса LinkedList, разместить в нём 3 ссылки на объекты класса Book. \n" +
+                "List< Book > list = new LinkedList< Book >();\n" +
+                "Вывести информацию о всех книгах. Программно удалить из коллекции вторую книгу, вывести на экран информацию о оставшихся книгах.");
+        List< Book > list = new LinkedList<>();
+        list.add(new Book("Sun","Olga",1990,15));
+        list.add(new Book("Moon","Tatiana",1915,145));
+        list.add(new Book("Winter","Pavel",1990,73));
+        System.out.println(list.toString());
+        list.remove(1);
+        System.out.println(list.toString());
+        System.out.println("2. Создать экземпляр класса HashSet, добавить в него 25 ссылок на экземпляры класса " +
+                "Book, предусмотреть не менее 6 одинаковых по значению книг. ");
+
+        Set< Book > set = new HashSet<>();
+        set.add(new Book("Sun","Olga","Migalevich","Evgen'evna",1990,15));
+        set.add(new Book("Moon","Tatiana","Push","SZdgbds",1915,1));
+        set.add(new Book("moon","Pavel","Gsadf","UKiz",1990,2));
+        set.add(new Book("Moon","Tatiana","Efgad","ZFBV",1915,3));
+        set.add(new Book("sun","Olga","SWssdf","Jsdad",1990,4));
+        set.add(new Book("Sun","Tatiana","Ogsdf","TASF",1990,5));
+        set.add(new Book("Sun","Olga","Migalevich","Evgen'evna",1990,15));
+        set.add(new Book("Sun","Olga","Migalevich","Evgen'evna",1990,15));
+        set.add(new Book("Winter","Pavel","GDSsd","ADF",1915,6));
+        set.add(new Book("winter","Olga","QWeEF","ADF",1990,7));
+        set.add(new Book("winter","Tatiana","sdfbgD","TAS",1915,8));
+        set.add(new Book("Winter","Olga","yeJD","SFgsfdt",1990,9));
+        set.add(new Book("Sun","Olga","iyoDFA","Wdfg",1990,15));
+        set.add(new Book("winter","Tatiana","ywH","YYFds",1990,10));
+        set.add(new Book("Sun","Olga","Migalevich","Evgen'evna",1990,15));
+        set.add(new Book("Sun","Olga","Migalevich","Evgen'evna",1990,15));
+        set.add(new Book("Sun","Olga","Migalevich","Evgen'evna",1990,15));
+        set.add(new Book("Sun","Olga","Migalevich","Evgen'evna",1990,15));
+        set.add(new Book("moon","Olga","asGFA","NSDs",1915,11));
+        set.add(new Book("Moon","Pavel","aergaer","Yda",1990,12));
+        set.add(new Book("moon","Pavel","aerhg","MasF",1990,13));
+        set.add(new Book("Moon","Pavel","etjDG","uDDD",1915,14));
+        set.add(new Book("Sun","Tatiana","afsdgADF","pAFFDSAB",1990,15));
+        set.add(new Book("sun","Olga","Migalevich","Evgen'evna",1990,16));
+        set.add(new Book("Sun","Tatiana","adfhgA","GSgs",1990,17));
+        set.add(new Book("sun","Pavel","BAD","Uadfbg",1915,18));
+
+        for (Book book : set) {
+            System.out.println(book.toString());
+        }
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("Вывести на экран информацию о книга" +
+                " (содержащихся в коллекции HashSet), название которых начинается с гласной буквы.");
+        for (Book book : set) {
+            if(book.getTitle().charAt(0)==book.getTitle().toUpperCase().charAt(0)){
+                System.out.println(book.toString());
+            }
+        }
+    }
+}
