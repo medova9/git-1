99895241613cf2299ab50ec099cc0a1b47ec0a4d
anka <medova9an9@gmail.com>
2019-11-14 21:43:49 +0300

    Сгенерировать несколько объектов данного класса, вывести список человек 1) в возрасте старше 20 лет; 2) посчитать средний возраст всех людей с именми, начинающимися на гласную букву 3) отфильтровать и вывести уникальные объекты в взрасте превышающий средний возраст всех людей

---
+++ src/streamIAPI/Tasks.java
@@ -0,0 +1,100 @@
+package streamIAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+
+public class Tasks {
+    public static void main(String[] args) {
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("Коля",40));
+        list.add(new Person("Оля",23));
+        list.add(new Person("Оля",23));
+        list.add(new Person("Петя",45));
+        list.add(new Person("Галя",30));
+        list.add(new Person("Коля",40));
+        list.add(new Person("Оксана",38));
+        list.add(new Person("Коля",63));
+
+        List<Person> list20 = list
+                .stream()
+                .filter(n -> n.getAge() > 20)
+                .collect(Collectors.toList());
+
+        System.out.println(list20);
+        Double average = list
+                .stream()
+                .filter(n -> (n.getName().toUpperCase().toCharArray()[0] + "").toLowerCase().matches("[уеыаоэяию]"))
+                .mapToInt((s) -> s.getAge())
+                .average().orElse(0);
+        System.out.println(average);
+
+        //3) отфильтровать и вывести уникальные объекты в взрасте превышающий средний возраст всех людей
+        Double old = list
+                .stream()
+                .mapToInt((s) -> s.getAge())
+                .average().orElse(0);
+        System.out.println(old);
+        List<Person> collect = list
+                .stream()
+                .filter(n -> n.getAge() > old)
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(collect);
+
+
+
+
+    }
+}
+class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+//gettersm setters, toString
+}
